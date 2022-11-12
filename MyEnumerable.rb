a2c771e34dae9ab3930069334e1b76b3454177c2
ZikaZaki <zika.github@gmail.com>
2022-11-12 18:05:14 +0300

    Add MyEnumerable module

---
+++ MyEnumerable.rb
@@ -0,0 +1,20 @@
+module MyEnumerable
+    def all?
+      result = true
+      each { |i| result = false unless yield i }
+      result
+    end
+  
+    def any?
+      result = false
+      each { |i| result = true if yield i }
+      result
+    end
+  
+    def filter
+      array = []
+      each { |i| array.push(i) if yield i }
+      array
+    end
+  end
+  
